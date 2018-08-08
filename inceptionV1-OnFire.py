62993b8ecf060aaa5de84540c59d08a1be0c74f1
tobybreckon <toby.breckon@cranfield.ac.uk>
2018-08-08 11:05:30 +0100

    create specific InceptionV1-OnFire example

---
+++ inceptionV1-OnFire.py
@@ -0,0 +1,179 @@
+################################################################################
+
+# Example : perform live fire detection in video using InceptionV1-OnFire CNN
+
+# Copyright (c) 2017/18 - Andrew Dunnings / Toby Breckon, Durham University, UK
+
+# License : https://github.com/tobybreckon/fire-detection-cnn/blob/master/LICENSE
+
+################################################################################
+
+import cv2
+import os
+import sys
+import math
+
+################################################################################
+
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d, avg_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.merge_ops import merge
+from tflearn.layers.estimator import regression
+
+################################################################################
+
+def construct_inceptionv1onfire (x,y):
+
+    # Build network as per arhcitecture in [Dunnings/Breckon, 2018]
+
+    network = input_data(shape=[None, y, x, 3])
+
+    conv1_7_7 = conv_2d(network, 64, 5, strides=2, activation='relu', name = 'conv1_7_7_s2')
+
+    pool1_3_3 = max_pool_2d(conv1_7_7, 3,strides=2)
+    pool1_3_3 = local_response_normalization(pool1_3_3)
+
+    conv2_3_3_reduce = conv_2d(pool1_3_3, 64,1, activation='relu',name = 'conv2_3_3_reduce')
+    conv2_3_3 = conv_2d(conv2_3_3_reduce, 128,3, activation='relu', name='conv2_3_3')
+
+    conv2_3_3 = local_response_normalization(conv2_3_3)
+    pool2_3_3 = max_pool_2d(conv2_3_3, kernel_size=3, strides=2, name='pool2_3_3_s2')
+
+    inception_3a_1_1 = conv_2d(pool2_3_3, 64, 1, activation='relu', name='inception_3a_1_1')
+
+    inception_3a_3_3_reduce = conv_2d(pool2_3_3, 96,1, activation='relu', name='inception_3a_3_3_reduce')
+    inception_3a_3_3 = conv_2d(inception_3a_3_3_reduce, 128,filter_size=3,  activation='relu', name = 'inception_3a_3_3')
+    inception_3a_5_5_reduce = conv_2d(pool2_3_3,16, filter_size=1,activation='relu', name ='inception_3a_5_5_reduce' )
+    inception_3a_5_5 = conv_2d(inception_3a_5_5_reduce, 32, filter_size=5, activation='relu', name= 'inception_3a_5_5')
+    inception_3a_pool = max_pool_2d(pool2_3_3, kernel_size=3, strides=1, )
+    inception_3a_pool_1_1 = conv_2d(inception_3a_pool, 32, filter_size=1, activation='relu', name='inception_3a_pool_1_1')
+
+    # merge the inception_3a__
+    inception_3a_output = merge([inception_3a_1_1, inception_3a_3_3, inception_3a_5_5, inception_3a_pool_1_1], mode='concat', axis=3)
+
+    inception_3b_1_1 = conv_2d(inception_3a_output, 128,filter_size=1,activation='relu', name= 'inception_3b_1_1' )
+    inception_3b_3_3_reduce = conv_2d(inception_3a_output, 128, filter_size=1, activation='relu', name='inception_3b_3_3_reduce')
+    inception_3b_3_3 = conv_2d(inception_3b_3_3_reduce, 192, filter_size=3,  activation='relu',name='inception_3b_3_3')
+    inception_3b_5_5_reduce = conv_2d(inception_3a_output, 32, filter_size=1, activation='relu', name = 'inception_3b_5_5_reduce')
+    inception_3b_5_5 = conv_2d(inception_3b_5_5_reduce, 96, filter_size=5,  name = 'inception_3b_5_5')
+    inception_3b_pool = max_pool_2d(inception_3a_output, kernel_size=3, strides=1,  name='inception_3b_pool')
+    inception_3b_pool_1_1 = conv_2d(inception_3b_pool, 64, filter_size=1,activation='relu', name='inception_3b_pool_1_1')
+
+    #merge the inception_3b_*
+    inception_3b_output = merge([inception_3b_1_1, inception_3b_3_3, inception_3b_5_5, inception_3b_pool_1_1], mode='concat',axis=3,name='inception_3b_output')
+
+    pool3_3_3 = max_pool_2d(inception_3b_output, kernel_size=3, strides=2, name='pool3_3_3')
+    inception_4a_1_1 = conv_2d(pool3_3_3, 192, filter_size=1, activation='relu', name='inception_4a_1_1')
+    inception_4a_3_3_reduce = conv_2d(pool3_3_3, 96, filter_size=1, activation='relu', name='inception_4a_3_3_reduce')
+    inception_4a_3_3 = conv_2d(inception_4a_3_3_reduce, 208, filter_size=3,  activation='relu', name='inception_4a_3_3')
+    inception_4a_5_5_reduce = conv_2d(pool3_3_3, 16, filter_size=1, activation='relu', name='inception_4a_5_5_reduce')
+    inception_4a_5_5 = conv_2d(inception_4a_5_5_reduce, 48, filter_size=5,  activation='relu', name='inception_4a_5_5')
+    inception_4a_pool = max_pool_2d(pool3_3_3, kernel_size=3, strides=1,  name='inception_4a_pool')
+    inception_4a_pool_1_1 = conv_2d(inception_4a_pool, 64, filter_size=1, activation='relu', name='inception_4a_pool_1_1')
+
+    inception_4a_output = merge([inception_4a_1_1, inception_4a_3_3, inception_4a_5_5, inception_4a_pool_1_1], mode='concat', axis=3, name='inception_4a_output')
+
+    pool5_7_7 = avg_pool_2d(inception_4a_output, kernel_size=5, strides=1)
+    pool5_7_7 = dropout(pool5_7_7, 0.4)
+    loss = fully_connected(pool5_7_7, 2,activation='softmax')
+    network = regression(loss, optimizer='momentum',
+                         loss='categorical_crossentropy',
+                         learning_rate=0.001)
+    model = tflearn.DNN(network, checkpoint_path='googlenetv2',
+                        max_checkpoints=1, tensorboard_verbose=2)
+
+    return model
+
+
+################################################################################
+
+# construct and display model
+
+model = construct_inceptionv1onfire (224, 224)
+print("Constructed InceptionV1-OnFire ...")
+
+model.load(os.path.join("models/InceptionV1-OnFire", "googlenetv2-7600"),weights_only=True)
+print("Loaded CNN network weights ...")
+
+################################################################################
+
+# network input sizes
+
+rows = 224
+cols = 224
+
+# display and loop settings
+
+windowName = "Live Fire Detection - InceptionV1-OnFire";
+keepProcessing = True;
+
+################################################################################
+
+if len(sys.argv) == 2:
+
+    # load video file from first command line argument
+
+    video = cv2.VideoCapture(sys.argv[1])
+    print("Loaded video ...")
+
+    # create window
+
+    cv2.namedWindow(windowName, cv2.WINDOW_NORMAL);
+
+    # get video properties
+
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH));
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = video.get(cv2.CAP_PROP_FPS)
+    frame_time = round(1000/fps);
+
+    while (keepProcessing):
+
+        # start a timer (to see how long processing and display takes)
+
+        start_t = cv2.getTickCount();
+
+        # get video frame from file, handle end of file
+
+        ret, frame = video.read()
+        if not ret:
+            print("... end of video file reached");
+            break;
+
+        # re-size image to network input size and perform prediction
+
+        small_frame = cv2.resize(frame, (rows, cols), cv2.INTER_AREA)
+        output = model.predict([small_frame])
+
+        # label image based on prediction
+
+        if round(output[0][0]) == 1:
+            cv2.rectangle(frame, (0,0), (width,height), (0,0,255), 50)
+            cv2.putText(frame,'FIRE',(int(width/16),int(height/4)),
+                cv2.FONT_HERSHEY_SIMPLEX, 4,(255,255,255),10,cv2.LINE_AA);
+        else:
+            cv2.rectangle(frame, (0,0), (width,height), (0,255,0), 50)
+            cv2.putText(frame,'CLEAR',(int(width/16),int(height/4)),
+                cv2.FONT_HERSHEY_SIMPLEX, 4,(255,255,255),10,cv2.LINE_AA);
+
+        # stop the timer and convert to ms. (to see how long processing and display takes)
+
+        stop_t = ((cv2.getTickCount() - start_t)/cv2.getTickFrequency()) * 1000;
+
+        # image display and key handling
+
+        cv2.imshow(windowName, frame);
+
+        # wait fps time or less depending on processing time taken (e.g. 1000ms / 25 fps = 40 ms)
+
+        key = cv2.waitKey(max(2, frame_time - int(math.ceil(stop_t)))) & 0xFF;
+        if (key == ord('x')):
+            keepProcessing = False;
+        elif (key == ord('f')):
+            cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
+else:
+    print("usage: python inceptionV1-OnFire.py videofile.ext");
+
+################################################################################
